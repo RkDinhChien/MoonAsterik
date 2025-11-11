84bfc27fb4c7bc6e0f506a47a3643138fb6d197f
Nguyen Minh Duc <23520312@uit.edu.vn>
2025-11-12 04:03:54 +0700

    fix button Employer

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -26,7 +26,7 @@ const renderLandingPage = () => `
             </svg>
             I'm a Student
           </button>
-          <button class="btn btn-lg btn-employer" onclick="app.handleEmployerClick()">
+          <button class="btn btn-primary btn-lg btn-employer" onclick="app.handleEmployerClick()">
             <svg class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor">
               <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
               <path d="M16 21V5a2 2 0 00-2-2h-4a2 2 0 00-2 2v16"></path>
